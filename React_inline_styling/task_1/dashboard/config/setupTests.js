1b60dd8d39b479769e6a8185e383a5a0f02d2c2a
Umugwaneza Cynthia <c.umugwanez@alustudent.com>
2025-01-28 17:45:08 +0200

    react

--- React_inline_styling/task_1/dashboard/config/setupTests.js
+++ React_inline_styling/task_1/dashboard/config/setupTests.js
@@ -1,5 +1,5 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import 'regenerator-runtime'; // Ensure compatibility with async functions if used in tests
+import 'regenerator-runtime/runtime'
 
 Enzyme.configure({ adapter: new Adapter() });
